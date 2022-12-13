0736fc3d8210e67ebea20e1d16d45413bd89e468
AidaDk1 <aidadosmuratkyzy@gmail.com>
2022-12-14 00:56:27 +0600

    Updates

---
+++ precache-manifest.88d2bb4eac7206aa6e0b62842deb51fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7241cd507e6e16006778d74137b8129",
+    "url": "/randomCountry1/index.html"
+  },
+  {
+    "revision": "f70ec40a2dd93a5538b1",
+    "url": "/randomCountry1/static/css/main.603f891f.chunk.css"
+  },
+  {
+    "revision": "b75377d708a3af48353b",
+    "url": "/randomCountry1/static/js/2.67d9d84c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/randomCountry1/static/js/2.67d9d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f70ec40a2dd93a5538b1",
+    "url": "/randomCountry1/static/js/main.b6942c70.chunk.js"
+  },
+  {
+    "revision": "b2cbd7d2b72912d08555",
+    "url": "/randomCountry1/static/js/runtime-main.8edede1f.js"
+  },
+  {
+    "revision": "d48a0b72bb03a93d634222fe2c2c5b16",
+    "url": "/randomCountry1/static/media/japan-flag.d48a0b72.jpg"
+  }
+]);
